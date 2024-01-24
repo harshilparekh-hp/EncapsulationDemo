bc36049c7f44d0e96513ad181387c47f02935268
harshil.parekh <harshil.parekh@STALAP-1126>
2024-01-24 09:45:57 -0600

    Add project files.

---
+++ EncapsulationDemo/Bank.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationDemo
+{
+    /// <summary>
+    /// Encapsulation hides the internal state and functionality of an object and only allows access thru public
+    /// set of functions/variables/properties.
+    /// </summary>
+    class Bank
+    {
+        public long AccountNumber;
+        //public string Name;
+
+        // auto-implemented property -
+        // this is the short-hand property and does the same thing as GetBalance() and SetBalance()
+        public string Name { get; set; }
+
+        // hiding class data by declaring the variable as private (by default the member is private)
+        int Balance;
+
+        public void Deposit()
+        {
+
+        }
+
+        //public getter method
+        public int GetBalance()
+        {
+            return Balance;
+        }
+
+        //public setter method
+        public void SetBalance(int balance)
+        {
+            //Validation while setting the balance to check if its positive else it will throw an exception.
+            if (balance > 0)
+                this.Balance = balance;
+            else
+                throw new Exception("Please pass a positive value");
+        }
+
+        public void Withdrawal()
+        {
+
+        }
+    }
+}
---
+++ EncapsulationDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            bank.AccountNumber = 123;
+            // bank.Balance // you cant access the balance amount as its hidden/private.
+            bank.Name = "CIBC Bank";
+            bank.SetBalance(500);
+            Console.WriteLine("My balance is : {0}", bank.GetBalance().ToString());
+            bank.SetBalance(150);
+            Console.WriteLine("My balance is : {0}", bank.GetBalance().ToString());
+            Console.WriteLine("name of the bank: {0}", bank.Name);
+
+            bank.Withdrawal();
+
+            Console.ReadKey();
+        }
+    }
+}
